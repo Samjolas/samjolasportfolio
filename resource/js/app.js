c3efb41e1e3b9d35b2159390eeda585890987e5f
Samuel Jolayemi <damoladfrosh@gmail.com>
2021-09-06 23:52:35 +0100

    Added new files

---
+++ resource/js/app.js
@@ -0,0 +1,15 @@
+/*const STORYBOX = document.querySelector("box1");
+const CHANGE = document.querySelector("#change-icon");
+
+function box() {
+    STORYBOX.style.backgroundcolor='black';
+    document.getElementById('red').style.color='red'
+}
+
+
+
+CHANGE.addEventListener("click", box, false);*/
+
+
+
+
